c24f15b5c303979980c0257c0975c0ba0637d4db
vicerascal <[vicerascal@nate.com]>
2018-07-02 15:44:30 +0900

    log add

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ steem.api.setOptions({url: 'https://api.steemit.com'});
 steem.api.getAccounts(['ned', 'dan'], function(err, response){
   console.log(err, response);
 });
+console.log("Good bye~!");
