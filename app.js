7236a1fd4fea0429207c372b9cf30be9345f718e
vicerascal <[vicerascal@nate.com]>
2018-07-02 15:57:03 +0900

    synchronize test

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 console.log("hello nodejs");
+
 var steem = require("steem")
 steem.api.setOptions({url: 'https://api.steemit.com'});
-steem.api.getAccounts(['ned', 'dan', 'mj-you'], function(err, response){
-  console.log(err, response);
-});
+
+var sync = require("synchronize");
+var fiber = sync.fiber;
+var await = sync.await;
+var defer = sync.defer;
+
+try{
+  fiber(function() {
+    var obj = await(steem.api.getAccounts(['ned', 'dan'], defer()));
+    console.log(obj);
+    obj = await(steem.api.getAccounts(['mj-you'], defer()));
+    console.log(obj);
+  });
+} catch(err){
+  console.logg(err);
+}
+
 console.log("Good bye~!");
