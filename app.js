ccf622e87dcc79470e2d68652c470f601fc6b53c
vicerascal <[vicerascal@nate.com]>
2018-07-04 15:05:57 +0900

    complete

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 var winston = require('winston');
 require('winston-daily-rotate-file');
 const tsFormat = () => (new Date()).toLocaleTimeString();
-var logger = winston.createLogger({
+var logger = new (winston.Logger)({
    transports: [
      new (winston.transports.Console)({
           timestamp: tsFormat
+        , level: 'info'
       }),
      new (winston.transports.DailyRotateFile)({
           // filename property 지정
@@ -72,6 +73,11 @@ var steem = require("steem")
 steem.api.setOptions({url: 'https://api.steemit.com'});
 // steem init! end
 
+var sync = require('synchronize');
+var fiber = sync.fiber;
+var await = sync.await;
+var defer = sync.defer;
+
 try {
     fiber(function() {
         // 실제 DB에 연결.
@@ -82,14 +88,14 @@ try {
         var queryResult = await(conn.query("select * from test", defer() ));
         logger.info(queryResult);
         // 테스트 데이터! 등록
-        queryResult = await(conn.query("insert into test values(1, 'nhj12311!!!');", defer() ));
+        queryResult = await(conn.query("insert into test values(1, 'mj-you');", defer() ));
         logger.info(queryResult);
         // 등록 후 다시 조회!!
         queryResult = await(conn.query("select * from test", defer() ));
         logger.info(queryResult);
         logger.info( "steem init!!" );
         // steem 데이터 조회!!
-        obj = await(steem.api.getAccounts(['nhj12311'], defer()));
+        obj = await(steem.api.getAccounts(['mj-you'], defer()));
         logger.info(obj);
         conn.end(); // db 연결 해제.
     });
